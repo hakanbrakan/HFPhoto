f2f110ada672a9b900426c72c4a26f71cfd95e85
inger <inger@192.168.1.6>
2020-11-10 21:59:43 +0100

    Class for guessing date from filename

---
+++ src/main/java/se/frihak/picture/PicturedateHelper.java
@@ -0,0 +1,40 @@
+package se.frihak.picture;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Pattern;
+
+import org.apache.commons.io.FilenameUtils;
+
+public class PicturedateHelper {
+	private static Pattern pattern1 = Pattern.compile("[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}\\.[0-9]{2}\\.[0-9]{2}");
+	private static Pattern pattern2 = Pattern.compile("[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}\\.[0-9]{2}\\.[0-9]{2}-[0-9]*");
+
+	public static String guessDateFromFilename(String filename) {
+		String filenameWithoutExtension = FilenameUtils.removeExtension(filename);
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH.mm.ss");
+		
+		if (pattern1.matcher(filenameWithoutExtension).find()) {
+			try {
+				LocalDate datum = LocalDate.parse(filenameWithoutExtension, formatter);
+				return datum.toString();
+			} catch (Exception e) {
+			}
+			
+		}
+
+		if (pattern2.matcher(filenameWithoutExtension).find()) {
+			try {
+				String test = filenameWithoutExtension.substring(0, 19);
+				LocalDate datum = LocalDate.parse(test, formatter);
+				return datum.toString();
+			} catch (Exception e) {
+			}
+			
+		}
+		
+		return "<datum saknas>";
+	}
+
+}
---
+++ src/test/java/se/frihak/picture/PicturedateHelperTest.java
@@ -0,0 +1,44 @@
+package se.frihak.picture;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class PicturedateHelperTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void bildPng() {
+		assertEquals("2020-10-29", PicturedateHelper.guessDateFromFilename("2020-10-29 17.07.01.png"));
+	}
+
+	@Test
+	public void filmMp4() {
+		assertEquals("2020-10-30", PicturedateHelper.guessDateFromFilename("2020-10-30 15.55.27.mp4"));
+	}
+
+	@Test
+	public void bildJpg() {
+		assertEquals("2020-10-30", PicturedateHelper.guessDateFromFilename("2020-10-30 18.04.44.jpg"));
+	}
+
+	@Test
+	public void bildJpg_2() {
+		assertEquals("2020-10-30", PicturedateHelper.guessDateFromFilename("2020-10-30 18.04.44-2.jpg"));
+	}
+
+	@Test
+	public void filmMov() {
+		assertEquals("2020-10-31", PicturedateHelper.guessDateFromFilename("2020-10-31 11.11.21.mov"));
+	}
+
+	@Test
+	public void invalidDate() {
+		assertEquals("<datum saknas>", PicturedateHelper.guessDateFromFilename("kallekula.mov"));
+	}
+}
